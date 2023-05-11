91f2c4234b228cabe2b7f6c002d3f836fbf13b8d
juba1508 <jule.bauer96@web.de>
2023-05-11 16:48:17 +0200

    add filter for invalide temperature

--- main.js
+++ main.js
@@ -70,9 +70,15 @@ L.geoJSON(jsondata, {
 
 function writeTemperatureLayer(jsondata){
     L.geoJSON(jsondata,{
+        filter: function(feature){
+            if (feature.properties.LT > -50 && feature.properties.LT < 50) {
+                return true;
+            }
+        },
         pointToLayer: function(feature, latlng) {
             return L.marker(latlng, {
                 icon: L.divIcon({
+                    className: "aws-div-icon",
                     html: `<span>${feature.properties.LT}</span>`
                 })
             });
