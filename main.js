a92ed0b88a02510e9e4b939401f561da6a78a19b
juba1508 <jule.bauer96@web.de>
2023-05-11 15:47:51 +0200

    add maxZoom

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ let layerControl = L.control.layers({
     "Relief avalanche.report": L.tileLayer(
         "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
         attribution: `© <a href="https://lawinen.report">CC BY avalanche.report</a>`
+        maxZoom: 12
     }).addTo(map),
     "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
     "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
@@ -51,12 +52,13 @@ async function showStations (url) {
         onEachFeature: function (feature, layer) {
             let prop = feature.properties; //Variable damit kürzer; * steht als Platzhalter für Bildunterschrift, Link für Infos, nur 1 Tab für Links
             let höhenmeter = feature.geometry.coordinates
+            let WG = if(prop.WG){return(pro.WG*3.6).toFixed(1)}else(return("keine Angabe"))
             layer.bindPopup(`      
             <h4>${prop.name} ${höhenmeter[2]} m ü NN</h4>
             <um>
             <li>Lufttemperatur (°C) ${prop.LT||"keine Angabe"}</li>
             <li>Relative Luftfeuchte (%) ${prop.RH||"keine Angabe"}</li>
-            <li>Windgeschwindigkeit (km/h) ${prop.WG||"keine Angabe"}</li>
+            <li>Windgeschwindigkeit (km/h) ${if(Number(prop.WG*3.6).toFixed(2))||"keine Angabe"}</li>
             <li>Schneehöhe (cm) ${prop.HS||"keine Angabe"}</li>
             </um>          
             `);
